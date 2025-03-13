922303f42e2b64c38c760ea269a94c9429e5cf88
Ali Usman Gouhar <aliusmangouhar@gmail.com>
2025-03-13 07:18:07 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align: center;">This is first project of git on github.</h1>
+    <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis cumque officia necessitatibus porro id rem, beatae enim aperiam illum nobis fuga praesentium iure provident itaque deleniti deserunt veniam soluta et similique fugiat totam non. Optio, accusamus vel repellendus ipsum voluptate ipsam, ad recusandae eaque expedita reiciendis quos ab, ratione quasi exercitationem placeat eligendi natus eos laboriosam sit odit enim veritatis dolorum? Quam at, aliquid est ad perferendis necessitatibus, fugit quibusdam ab animi minus ullam quae quidem nihil praesentium nemo! Omnis eligendi iusto laboriosam sit ad eaque praesentium assumenda corporis suscipit ab aliquid, voluptate aut ipsa magnam minima aperiam quia odio.</p>
+</body>
+</html>
